84b9febde1ac459f07504666a648d245cd7df984
kishan <srkishan2407@gmail.com>
2021-06-27 19:11:58 +0530

    UUID and token generation

--- server.js
+++ server.js
@@ -4,18 +4,23 @@ var express = require("express"),
   path = require("path"),
   cors = require("cors");  
 
+  bodyParser = require("body-parser");
+
+
 
 
 const PORT = 3000;
-
-
 var app = express();
-
 var corsOptions = {
   origin: "http://localhost:"+PORT
 };
 
 app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = require("./models");
 db.mongoose
@@ -31,17 +36,15 @@ db.mongoose
     process.exit();
   });
 
-
 app.get("/", (req, res) => {
   res.json({
     message: "Welcome to Upgrad Movie booking application development.",
   });
 });
-
 require("./routes/movie.routes")(app);
 require("./routes/artist.routes")(app);
 require("./routes/genre.routes")(app);
-// require("./routes/user.routes")(app)
+require("./routes/user.routes")(app)
 
 app.listen(PORT, function () {
   console.log("express has started on port ", PORT);
