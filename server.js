7d38ee6f69d739e0cd57a433441ca7a2941dbeeb
kishan <srkishan2407@gmail.com>
2021-06-27 18:41:52 +0530

    Defining the schema and Setting up the index file

--- server.js
+++ server.js
@@ -5,21 +5,30 @@ var express = require('express')
 
 var app = express();
 
+const db = require("./models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
 
 app.get('/movie',function(req,res){
     res.send("All Movies Data in JSON format from Mongo DB")
 })
-
 app.get('/genres',function(req,res){
     res.send("All Genres Data in JSON format from Mongo DB")
 })
-
 app.get('/artists',function(req,res){
     res.send("All Artists Data in JSON format from Mongo DB")
 })
-
-
-
 app.listen(9000, function () {
     console.log("express has started on port 3000");
    });
