b4ec7691519c31ba98cbc732a8241ec9e54302a0
kishan <srkishan2407@gmail.com>
2021-06-27 18:56:08 +0530

    Express loaded and API and routes are coded in controller

--- server.js
+++ server.js
@@ -1,34 +1,48 @@
-var express = require('express')
-  ,http = require('http')
-  ,fs = require('fs')
-  ,path = require('path');
+var express = require("express"),
+  http = require("http"),
+  fs = require("fs"),
+  path = require("path"),
+  cors = require("cors");  
+
+
+
+const PORT = 3000;
+
 
 var app = express();
+
+var corsOptions = {
+  origin: "http://localhost:"+PORT
+};
+
+app.use(cors(corsOptions));
 
 const db = require("./models");
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
     console.log("Connected to the database!");
-
   })
-  .catch(err => {
+  .catch((err) => {
     console.log("Cannot connect to the database!", err);
     process.exit();
   });
 
-app.get('/movie',function(req,res){
-    res.send("All Movies Data in JSON format from Mongo DB")
-})
-app.get('/genres',function(req,res){
-    res.send("All Genres Data in JSON format from Mongo DB")
-})
-app.get('/artists',function(req,res){
-    res.send("All Artists Data in JSON format from Mongo DB")
-})
-app.listen(9000, function () {
-    console.log("express has started on port 3000");
-   });
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to Upgrad Movie booking application development.",
+  });
+});
+
+require("./routes/movie.routes")(app);
+require("./routes/artist.routes")(app);
+require("./routes/genre.routes")(app);
+// require("./routes/user.routes")(app)
+
+app.listen(PORT, function () {
+  console.log("express has started on port ", PORT);
+});
